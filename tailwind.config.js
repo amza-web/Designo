74c9e3b344106b5d53cf9f20faf5e998f06e8537
Amza <amza.web@gmail.com>
2023-06-21 23:38:39 +0100

    added a font and an extra color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jost: 'Jost, sans-serif',
+      },
+      colors: {
+        peach: "#e88069"
+      },     
+    },
   },
   plugins: [],
 }
