527ca1e2441104048ddca5e3a4d03fc47cbd4453
Amza <amza.web@gmail.com>
2023-06-24 10:53:55 +0100

    fixed a little problem preventing deployment

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -5,7 +5,7 @@ const CallToAction = () => {
   return (
     <div className='relative z-10 bg-peach text-white text-center flex justify-center items-center px-[27px] md:px-6 py-16 rounded-[15px]'>
         <div>
-            <h1>Let's talk about your project</h1>
+            <h1>Let&apos;'s talk about your project</h1>
             <p> Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
             <Link className='inline-block btn mt-8' href="#">Get in touch</Link>
         </div>
