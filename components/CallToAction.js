2c5fa311a8ad4d9fe832d5550e0707ab3cde4e75
Amza <amza.web@gmail.com>
2023-06-24 10:46:28 +0100

    changed some styling

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const CallToAction = () => {
   return (
-    <div className='bg-peach text-white text-center flex justify-center items-center px-[27px] py-16 rounded-[15px]'>
+    <div className='relative z-10 bg-peach text-white text-center flex justify-center items-center px-[27px] md:px-6 py-16 rounded-[15px]'>
         <div>
             <h1>Let's talk about your project</h1>
             <p> Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
