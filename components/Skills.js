d5c005a54d899be4470b697ec5b9d59e0e6cdb4f
Amza <amza.web@gmail.com>
2023-06-24 10:50:05 +0100

    made little adjustments for tablet screen

--- components/Skills.js
+++ components/Skills.js
@@ -10,7 +10,7 @@ import CallToAction from './CallToAction'
 
 const Skills = () => {
   return (
-    <section className='container flex flex-col relative text-center'>
+    <section className='container flex flex-col relative text-center -mb-24  md:max-w-[25.5rem]'>
         <div className='mb-11'>
             <div className='flex justify-center items-center mb-[43px]'>
                 <Image className='absolute z-0' src={imgBg} />
@@ -27,8 +27,8 @@ const Skills = () => {
                 <Image className='z-10 relative' src={img2} />
             </div>
             <div>
-                <h3 className='text-xl leading-[26px] tracking-[5px] uppercase font-medium mb-8'>Passionate</h3>
-                <p>Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.</p>
+                <h3 className='text-xl leading-[26px] tracking-[5px] uppercase font-medium mb-8'>Resourceful</h3>
+                <p>Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients' needs.</p>
             </div>
         </div>
         <div className='mb-11'>
@@ -37,8 +37,8 @@ const Skills = () => {
                 <Image className='z-10 relative' src={img3} />
             </div>
             <div>
-                <h3 className='text-xl leading-[26px] tracking-[5px] uppercase font-medium mb-8'>Passionate</h3>
-                <p>Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.</p>
+                <h3 className='text-xl leading-[26px] tracking-[5px] uppercase font-medium mb-8'>Friendly</h3>
+                <p>We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.</p>
             </div>
         </div>
         <CallToAction />
