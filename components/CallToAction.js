39cf80e69fc17e709c5de1aeb65f2ce918a5d221
Amza <amza.web@gmail.com>
2023-06-25 02:51:25 +0100

    made some adjustments for responsive styling

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -3,11 +3,13 @@ import Link from 'next/link'
 
 const CallToAction = () => {
   return (
-    <div className='relative z-10 bg-peach text-white text-center flex justify-center items-center px-[27px] md:px-6 py-16 rounded-[15px]'>
-        <div>
-            <h1>Let&apos;'s talk about your project</h1>
-            <p> Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
-            <Link className='inline-block btn mt-8' href="#">Get in touch</Link>
+    <div className='relative z-10 bg-peach text-white text-center flex justify-center items-center px-[27px] md:px-6 py-16 lg:py-[72px] lg:px-24 rounded-[15px]'>
+        <div className='lg:flex lg:justify-between lg:w-full lg:items-center'>
+            <div className='lg:text-left lg:max-w-md'>
+              <h1 className='lg:text-[40px] lg:max-w-[270px]'>Let's talk about your project</h1>
+              <p> Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
+            </div>
+            <Link className='inline-block btn mt-8 h-min lg:mt-0' href="#">Get in touch</Link>
         </div>
     </div>
   )
