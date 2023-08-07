6fca049abef1b712950fbfd84c98037b95efcf26
Amza <amza.web@gmail.com>
2023-08-07 06:14:50 +0100

    created project card component

---
+++ components/ProjectInd.js
@@ -0,0 +1,28 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
+
+import arrow from "@/app/assets/shared/desktop/icon-right-arrow.svg"
+
+const ProjectInd = ({details}) => {
+  const router = useRouter()
+  return (
+    <div onClick={() => router.push(details.link)} className='text-white flex justify-center items-center text-center grid-web relative  group rounded-[15px] overflow-hidden'>
+        <div className='bg-black absolute w-full h-full bg-opacity-50 transition-colors duration-500 group-hover:bg-peach group-hover:bg-opacity-50'></div>
+        
+        <Image className='w-full hidden lg:block' src={details.lgWeb} alt='Web'/> 
+        <Image className='w-full lg:hidden' src={details.web} alt='Web' />
+        
+        <div className='absolute uppercase'>
+            <h2 className='text-[28px] leading-9 mb-7 lg:text-[40px] tracking-[1.4px] lg:tracking-[2px]'>{details.title}</h2>
+            <Link className='inline-flex justify-center items-center gap-4  tracking-[5px] text-[15px] font-medium' href={details.link}>View Projects <Image height={10} src={arrow} alt='arrow' /></Link>
+        </div>
+
+    </div>
+  )
+}
+
+export default ProjectInd
