6c8fb4e3651a2cba8f8fb0a6f81b9b6016578172
Amza <amza.web@gmail.com>
2023-08-17 02:16:29 +0100

    started locations page

---
+++ app/locations/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+
+export default function Locations() {
+  return (
+    <>
+        <Header />
+        <main>
+
+        </main>
+        <Footer />
+    </>
+  )
+}
