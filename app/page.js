b25a293e87cc839037a49be4a97b1f3365ef1acd
Amza Omiteru <amza.web@gmail.com>
2023-09-05 15:42:13 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,11 @@ import Hero from '@/components/Hero'
 import Projects from '@/components/Projects'
 import Skills from '@/components/Skills'
 
+export const metadata = {
+  title: 'Designo',
+  description: 'Landing page of the designo website',
+}
+
 export default function Home() {
   return (
     <>  
